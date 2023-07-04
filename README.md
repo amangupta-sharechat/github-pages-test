462620eb40b40d23e45b4ab2137b9e9cb1ac42fb
Aman Gupta <amangupta@sharechat.co>
2023-07-04 17:17:26 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# GithubPagesTest
+
+A description of this package.
---
+++ Sources/GithubPagesTest/GithubPagesTest.swift
@@ -0,0 +1,6 @@
+public struct GithubPagesTest {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
